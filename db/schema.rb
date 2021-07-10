20c328506b038ac72375d7b02e9002240cc20850
masaakiKanno850 <masaaki.kanno.pr@gmail.com>
2021-07-10 14:47:37 +0000

    statsの型をstringに修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_045911) do
+ActiveRecord::Schema.define(version: 2021_07_10_140501) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "status"
+    t.string "status"
   end
 
 end
